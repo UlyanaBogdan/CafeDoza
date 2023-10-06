8653c32925a0bf01e4a29237b5c58a188a53414f
UlyanaBogdan <u.bogdanlesnikova@gmail.com>
2023-10-07 02:59:16 +0300

    refactoring the code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const browserSync = require("browser-sync").create();
 
 browserSync.init({
-    "server": "app",
-    "port": 9000,
+    "server": ["app/src"],
+    "startPath": "index.html",
+    "port": 3000,
     "proxy": false,
-    "files": ["app/*.html", "app/src/css/*.css", "app/src/js/*.js"],
+    "files": ["app/src/index.html", "app/src/css/*.css", "app/src/**/*.js", "app/src/img/*.png", "app/src/img/*.jpeg"],
     "ghostMode": false,
     "reloadDelay": 10,
     "reloadDebounce": 800,
@@ -13,3 +14,34 @@ browserSync.init({
     "open": true,
     "notify": false,
 });
+
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var reload      = browserSync.reload;
+//
+// // Save a reference to the reload method
+//
+// // Watch scss AND html files, doing different things with each.
+// gulp.task('serve', function () {
+//
+//     // Serve files from the root of this project
+//     browserSync.init({
+//         // server: {
+//         //     baseDir: "./",
+//         // },
+//         "server": ["app/src"],
+//         "startPath": "index.html",
+//         "port": 3000,
+//         "proxy": false,
+//         "files": ["app/src/index.html", "app/src/css/*.css", "app/src/**/*.js", "app/src/img/*.png", "app/src/img/*.jpeg"],
+//         "ghostMode": false,
+//         "reloadDelay": 10,
+//         "reloadDebounce": 800,
+//         "injectChanges": false,
+//         "minify": false,
+//         "open": true,
+//         "notify": false,
+//     });
+//
+//     gulp.watch("*.html").on("change", reload);
+// });
---
+++ app/src/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://*; child-src 'none';">
+    <title>Title</title>
+<meta http-equiv="Content-Security-Policy" content="
+        default-src 'self';
+        style-src-elem 'self' https://fonts.googleapis.com;
+        font-src https://fonts.gstatic.com;
+    ">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <!--    <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">-->
+            <link rel="stylesheet" href="./css/style.css">
+                <link rel="stylesheet" href="./css/animate.min.css">
+                </head>
+                <body>
+                <div id="app" class="app"></div>
+                <script defer type="text/javascript" src="./js/wow.min.js"></script>
+                <script defer type="text/javascript" src="./js/components.js"></script>
+                <script defer type="text/javascript" src="./js/pages.js"></script>
+                <script defer src="./js/javascript.js" type="module"></script>
+                </body>
+            </html>
