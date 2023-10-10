90bbe34449229e0358144016ed48e221ffd0128e
UlyanaBogdan <u.bogdanlesnikova@gmail.com>
2023-10-11 01:47:27 +0300

    wip QR scan test

--- app/src/components/AdminCabinet.js
+++ app/src/components/AdminCabinet.js
@@ -3,16 +3,15 @@ const AdminCabinet = {
         return `
             <section class="admin-cabinet">
                 <div class="qr-scan-block">
-                    <button type="button" id="scan-qr-btn" class="scan-qr-btn closed">TAP TO SCAN QR</button>
+                    <button type="button" id="scan-qr-btn" class="scan-qr-btn">TAP TO SCAN QR</button>
                     <div id="reader"></div>
-                    <div class="inputblock-admin">
+                    <div class="inputblock-admin closed">
                         <button type="button" class="remove-cups"></button>
                         <input type="number" class="input-admin">
                         <button type="button" class="add-cups"></button>
                     </div>
-                    <btn class="addbtn-admin" id="addbtn-admin">Add</btn>
-                    <btn class="removebtn-admin" id="removebtn-admin">Remove</btn>
-                    
+                    <btn class="addbtn-admin closed" id="addbtn-admin">Add</btn>
+                    <btn class="removebtn-admin closed" id="removebtn-admin">Remove</btn>
                 </div>
             </section>
             `;
