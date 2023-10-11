04f8bfa98efd3ccf2bec6b620c94b795312ff0d1
UlyanaBogdan <u.bogdanlesnikova@gmail.com>
2023-10-12 01:51:53 +0300

    TEST REG

--- app/src/config.js
+++ app/src/config.js
@@ -2,6 +2,6 @@ export const getUrls = {
     //TODO
     // loginURL: ,
     // registerURL: ,
-    baseURL: "https://woofwoof.space/api/v1",
+    baseURL: "https://woofwoof.space/test/api/v1",
     regURL: "/registration"
 }
