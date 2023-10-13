0ad516ca4a7169b6fc1242db50da355d27e55d06
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-13 09:57:45 +0200

    check disable double click

--- app/src/config.js
+++ app/src/config.js
@@ -2,7 +2,7 @@ export const getUrls = {
     //TODO
     // loginURL: ,
     // registerURL: ,
-    baseURL: "localhost:8090/api/v1",
+    baseURL: "woofwoof.space/api/v1",
     regURL: "/registration",
     roleURL: "/role",
     loginURL: "/login",
