de005b51216b4dc8dc8a22e1a71463ed2d2dfa66
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-17 11:34:20 +0200

    google button wip

--- app/src/components/ModalAuth.js
+++ app/src/components/ModalAuth.js
@@ -15,6 +15,26 @@ const ModalAuth = {
           <button type="button" class="log-btn log-part lng">sign in</button>
           <button type="button" class="have-acc reg-part closed lng">Already have an account?</button>
           <button type="button" class="new-here log-part lng">New here?</button>
+          <button type="button" class="google-signin">
+          <div class="google-signin"><div id="g_id_onload"
+             data-client_id="1072002311281-avq0c6im5urd2avgor0h57437ogdoidq.apps.googleusercontent.com"
+             data-context="signin"
+             data-ux_mode="popup"
+             data-login_uri="http://localhost:63343"
+             data-auto_select="true"
+             data-itp_support="true">
+          </div>
+
+            <div class="g_id_signin"
+                 data-type="standard"
+                 data-shape="rectangular"
+                 data-theme="filled_black"
+                 data-text="continue_with"
+                 data-size="medium"
+                 data-locale="en-US"
+                 data-logo_alignment="left">
+            </div>
+</button>
         </div>
         <div class="successful-reg-modal closed">
           <h3 class="lng">Congratulations!<br>Successful registration!<br>♥</h3>
